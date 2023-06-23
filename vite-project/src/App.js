95bc6d36774d4edab3b6800e25c0112372f7c5ae
kkhushitolanii <kkhushitolanii@gmail.com>
2023-06-24 04:59:12 +0530

    first commit

---
+++ vite-project/src/App.js
@@ -0,0 +1,61 @@
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Listing from "./pages/Listing";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
+        </Routes>
+      </Router>
+    <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+/>
+    </>
+  );
+}
+
+export default App;
